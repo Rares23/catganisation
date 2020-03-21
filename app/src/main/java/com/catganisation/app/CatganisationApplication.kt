9f02ec09c35d483fca3092368656d6fb440271e8
Rares Cruceat <cruceat.rares@gmail.com>
2020-03-21 16:39:42 +0200

    create and setup app component in main application class

---
+++ app/src/main/java/com/catganisation/app/CatganisationApplication.kt
@@ -0,0 +1,23 @@
+package com.catganisation.app
+
+import android.app.Application
+import com.catganisation.di.components.AppComponent
+import com.catganisation.di.components.DaggerAppComponent
+import com.catganisation.di.modules.AppModule
+
+class CatganisationApplication : Application()  {
+
+    private lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule())
+            .build()
+    }
+
+    fun getAppComponent() : AppComponent {
+        return appComponent
+    }
+}
