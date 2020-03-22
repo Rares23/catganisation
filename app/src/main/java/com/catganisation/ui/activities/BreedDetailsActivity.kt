f09545d8c4cc83ca8f42f1f5cd5a4e03858faf28
Rares Cruceat <cruceat.rares@gmail.com>
2020-03-22 16:39:08 +0200

    create breed details activity

---
+++ app/src/main/java/com/catganisation/ui/activities/BreedDetailsActivity.kt
@@ -0,0 +1,36 @@
+package com.catganisation.ui.activities
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.catganisation.R
+import kotlinx.android.synthetic.main.toolbar.*
+
+class BreedDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_breed_details)
+        initializeUi()
+    }
+
+    private fun initializeUi() {
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        toolbar.title = getString(R.string.details_page_title)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
